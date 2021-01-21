67873ae41688b7c327bcc6d0f30f8e3732bcbe11
Mubassir-Ali <mubassir11@gmail.com>
2021-01-21 10:17:58 +0500

    create folder structure for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 
-import './App.css';
+// import Cards from './Components/Cards/Cards'
+// import Chart from './Components/Chart/Chart'
+// import CountryPicker from './Components/CountryPicker/CountryPicker'
+
+import {Cards,Chart,CountryPicker} from './Components';
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.container}>
+    <Cards/>
+    <CountryPicker/>
+    <Chart/>
+    
       
     </div>
   );
